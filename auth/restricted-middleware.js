b9b1409951144bf7cdc8e9052f713d2d3fa0cdba
VanOrder <vanorder@VanOrders-iMac.local>
2019-08-20 13:21:53 -0800

    mvp met for two day assignment wiht log in, log out, and authentication using cookies and sessions

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,25 +1,7 @@
-const bcrypt = require('bcryptjs');
-
-const Users = require('../users/users-model.js');
-
-
 module.exports =  function restricted( req, res, next) {
-    let { username, password } = req.headers;
-
-    if(username && password) {
-        Users.findBy({ username })
-        .first()
-        .then(user => {
-            if(user && bcrypt.compareSync(password, user.password)) {
-                next();
-            } else {
-                res.status(401).json({ message: 'You shall not pass!' })
-            }
-        })
-        .catch(err => {
-            res.status(500).json(error)
-        })
+    if(req.session && req.session.user) {
+        next();
     } else {
         res.status(400).json({ message: 'Give us legit creds' })
     }
-}
+};
--- server.js
+++ server.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
+const session = require('express-session');
+const helmet = require('helmet');
+const KnexSessionStore = require('connect-session-knex')(session);
+
 const Users = require('./users/users-model.js');
 const restricted = require('./auth/restricted-middleware.js');
+const knexConnection = require('./data/db-config.js');
 
 const server = express();
 
+const sessionConfig = {
+  name: 'starFox',
+  secret: process.env.COOKIE_SECRET || 'I cant let you see that, starFox',
+  cookie: {
+    maxAge: 1000 * 60,
+    secure: process.env.COOKIE_SECURE || false, //make this true in production
+    httpOnly: true,
+  },
+  resave: false,
+  saveUninitialized: false, //illegal to automatically set cookies
+  store: new KnexSessionStore({
+    knex: knexConnection,
+    createtable: true,
+    clearInterval: 1000 * 60 * 60,
+  }),
+};
+
+server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig));
 
 server.get('/', (req, res) => {
   res.send("We have lift off");
@@ -28,15 +52,16 @@ server.post('/api/register', (req, res) => {
     });
 });
 
-server.post('/api/login', restricted, (req, res) => {
-  let { username, password } = req.body;
+server.post('/api/login', (req, res) => {
+  let { username, password }= req.body;
 
   Users.findBy({ username })
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.user = user;
+        req.session.loggedIn = true;
         res.status(200).json({ message: `Welcome ${user.username}!` });
-        localStorage.setItem('userLoggedIn', true)
       } else {
         res.status(401).json({ message: 'You shall not PASS!!!' });
       }
@@ -52,6 +77,12 @@ server.get('/api/users', restricted, (req, res) => {
       res.json(users);
     })
     .catch(err => res.send(err));
+});
+
+server.get('/api/logout', (req, res) => {
+  req.session.destroy(() => {
+    res.status(200).json({ adios: 'muchacho' });
+  });
 });
 
 module.exports = server;
